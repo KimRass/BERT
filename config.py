fefaba65c6f9d96dea653ca2dd9538d2584a8b6b
KimRass <rmx1000@yonsei.ac.kr>
2023-08-30 22:34:36 +0900

    EPUBTXT_DIR

--- config.py
+++ config.py
@@ -1,20 +1,18 @@
 import torch
+from pathlib import Path
 
 ### Data
 VOCAB_SIZE = 30_522
 MAX_LEN = 512
 ### BookCorpus
+# EPUBTXT_DIR = "/home/user/cv/bookcorpus/epubtxt"
 EPUBTXT_DIR = "/Users/jongbeomkim/Documents/datasets/bookcorpus/epubtxt"
-# config.EPUBTXT_DIR = "/Users/jongbeomkim/Documents/datasets/bookcorpus_subset/epubtxt"
-VOCAB_PATH = "/Users/jongbeomkim/Desktop/workspace/bert_from_scratch/pretrain/bookcorpus_vocab.json"
+# VOCAB_PATH = "/Users/jongbeomkim/Desktop/workspace/bert_from_scratch/pretrain/bookcorpus_vocab.json"
+VOCAB_PATH = Path(__file__).parent/"pretrain/bookcorpus_vocab.json"
 MIN_FREQ = 5
 
 ### Architecture
 DROP_PROB = 0.1 # "For the base model, we use a rate of $P_{drop} = 0.1$."
-# D_MODEL = 512
-# N_HEADS = 8
-# N_LAYERS = 6
-# HIDDEN_SIZE = 768
 
 ### Optimizer
 MAX_LR = 1e-4
