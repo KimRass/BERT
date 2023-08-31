b81866e7908255d017b587ef86810df56552ba53
KimRass <rmx1000@yonsei.ac.kr>
2023-08-31 17:31:04 +0900

    random.uniform->random.randrange

--- pretrain/bookcorpus.py
+++ pretrain/bookcorpus.py
@@ -64,7 +64,7 @@ class BookCorpusForBERT(Dataset):
             next_idx = idx + 1
             is_next = 1
         else:
-            next_idx = random.uniform(0, len(self.ls_token_ids) - 1)
+            next_idx = random.randrange(len(self.ls_token_ids))
             is_next = 0
         next_token_ids = self.ls_token_ids[next_idx]
         return next_token_ids, torch.as_tensor(is_next)
