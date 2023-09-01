cb3931e0c321e74a756a9952fa64fe07a208e383
KimRass <rmx1000@yonsei.ac.kr>
2023-09-01 14:41:36 +0900

    Tokenize on-the-fly

--- pretrain/bookcorpus.py
+++ pretrain/bookcorpus.py
@@ -39,7 +39,6 @@ class BookCorpusForBERT(Dataset):
         self.unk_id = tokenizer.token_to_id("[UNK]")
 
         self._parse()
-        self._tokenize()
 
     def _parse(self):
         print("Parsing BookCorpus...")
@@ -51,18 +50,11 @@ class BookCorpusForBERT(Dataset):
                     self.parags.append(parag)
         print("Completed")
 
-    def _tokenize(self):
-        print("Tokenizing BookCorpus...")
-        self.ls_token_ids = list()
-        for idx in tqdm(range(0, len(self.parags), self.chunk_size)):
-            encoded = self.tokenizer.encode_batch(self.parags[idx: idx + self.chunk_size])
-            self.ls_token_ids.extend([i.ids[1: -1] for i in encoded])
-        print("Completed")
-
-    def _to_bert_input(self, token_ids, next_token_ids):
+    def _to_bert_input(self, cur_token_ids, next_token_ids):
         token_ids = (
-            [self.cls_id] + token_ids[: self.max_len - 3] + [self.sep_id] + next_token_ids
+            [self.cls_id] + cur_token_ids[: self.max_len - 3] + [self.sep_id] + next_token_ids
         )[: self.max_len - 1] + [self.sep_id]
+        ### Pad
         token_ids += [self.pad_id] * (self.max_len - len(token_ids))
         return torch.as_tensor(token_ids)
 
@@ -71,10 +63,10 @@ class BookCorpusForBERT(Dataset):
             next_idx = idx + 1
             is_next = 1
         else:
-            next_idx = random.randrange(len(self.ls_token_ids))
+            next_idx = random.randrange(len(self.parags))
             is_next = 0
-        next_token_ids = self.ls_token_ids[next_idx]
-        return next_token_ids, torch.as_tensor(is_next)
+        next_parag = self.parags[next_idx]
+        return next_parag, torch.as_tensor(is_next)
 
     def _token_ids_to_segment_ids(self, token_ids):
         seg_ids = torch.zeros_like(token_ids, dtype=token_ids.dtype, device=token_ids.device)
@@ -85,14 +77,16 @@ class BookCorpusForBERT(Dataset):
         return seg_ids
 
     def __len__(self):
-        return len(self.ls_token_ids) - 1
+        return len(self.parags) - 1
 
     def __getitem__(self, idx):
-        token_ids = self.ls_token_ids[idx]
-        next_token_ids, is_next = self._sample_next_sentence(idx)
+        parag = self.parags[idx]
+        cur_token_ids = self.tokenizer.encode(parag).ids[1: -1]
+        next_parag, is_next = self._sample_next_sentence(idx)
+        next_token_ids = self.tokenizer.encode(next_parag).ids[1: -1]
 
         token_ids = self._to_bert_input(
-            token_ids=token_ids, next_token_ids=next_token_ids,
+            cur_token_ids=cur_token_ids, next_token_ids=next_token_ids,
         )
         seg_ids = self._token_ids_to_segment_ids(token_ids)
         return token_ids, seg_ids, is_next
