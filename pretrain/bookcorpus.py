3d3e6ff5c41d7440a731ee03e8b3197e0288c3cc
KimRass <rmx1000@yonsei.ac.kr>
2023-09-04 01:38:57 +0900

    Lower chunk size

--- pretrain/bookcorpus.py
+++ pretrain/bookcorpus.py
@@ -24,7 +24,7 @@ class BookCorpusDataset(Dataset):
         epubtxt_dir,
         tokenizer,
         seq_len,
-        tokenize_in_advance=False,
+        tokenize_in_advance=True,
         chunk_size=2 ** 6,
     ):
         self.epubtxt_dir = epubtxt_dir
