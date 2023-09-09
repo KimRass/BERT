5ed694763561df0440f996aed998722122b32363
KimRass <rmx1000@yonsei.ac.kr>
2023-09-10 01:48:23 +0900

    Seg id only zero if one SEP

--- utils.py
+++ utils.py
@@ -20,8 +20,8 @@ def print_number_of_parameters(model):
 def _token_ids_to_segment_ids(token_ids, sep_id):
     seg_ids = torch.zeros_like(token_ids, dtype=token_ids.dtype, device=token_ids.device)
     is_sep = (token_ids == sep_id)
-    # if is_sep.sum() == 2:
-    first_sep, second_sep = is_sep.nonzero()
-    # The positions from right after the first '[SEP]' token and to the second '[SEP]' token
-    seg_ids[first_sep + 1: second_sep + 1] = 1
+    if is_sep.sum() == 2:
+        first_sep, second_sep = is_sep.nonzero()
+        # The positions from right after the first '[SEP]' token and to the second '[SEP]' token
+        seg_ids[first_sep + 1: second_sep + 1] = 1
     return seg_ids
