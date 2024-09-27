d658d9eacc418f65c47e9c71aea212ae8c716eb2
KimRass <rmx1000@yonsei.ac.kr>
2024-09-27 17:10:44 +0900

    Minor changes

--- model.py
+++ model.py
@@ -229,7 +229,6 @@ class BERT(nn.Module):
         acc = (gt_is_next == argmax).float().mean()
         return acc.item()
 
-
     def get_mlm_acc(pred_token_ids, gt_token_ids):
         argmax = torch.argmax(pred_token_ids, dim=2)
         acc = (gt_token_ids == argmax).sum() / gt_token_ids.numel()
